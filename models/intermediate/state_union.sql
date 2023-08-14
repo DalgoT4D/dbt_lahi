b61ffafd229de5c09f7ed9a91748f75c33ab52ae
Abhishek N <abhisheknshetty97@gmail.com>
2023-08-14 23:55:34 +0530

    add prefix and suffix to column names (#3)
    * add prefix and suffix to column names
    
    * flatten all the models in the db
    
    * rename normalized to state_union

---
+++ models/intermediate/state_union.sql
@@ -0,0 +1,27 @@
+{{ config(
+  materialized='table',
+   indexes=[
+      {'columns': ['_airbyte_ab_id'], 'type': 'hash'}
+    ],
+    schema='intermediate'
+
+) }}
+
+-- Creating a CTE that flattens the JSON data from the raw_enrollment table
+
+
+({{
+    flatten_json(
+        model_names = [source('source_lahi', 'andhra_pradesh_database'),
+                        source('source_lahi', 'assam_database'), 
+                        source('source_lahi', 'chattisgarh_database'), 
+                        source('source_lahi', 'delhi_database'), 
+                        source('source_lahi', 'gujarat_database'), 
+                        source('source_lahi', 'himachal_pradesh_database'), 
+                        source('source_lahi', 'karnataka_database'), 
+                        source('source_lahi', 'maharastra_database'), 
+                ],
+        json_column = '_airbyte_data'
+    )
+}})
+
