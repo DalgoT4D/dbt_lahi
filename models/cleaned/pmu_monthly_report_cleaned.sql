2b8789ab5b096c86cbb6bae3505f9e3582d27a22
Abhishek-N <abhisheknshetty97@gmail.com>
2023-08-29 14:12:59 +0530

    add sum of each columns

--- models/cleaned/pmu_monthly_report_cleaned.sql
+++ models/cleaned/pmu_monthly_report_cleaned.sql
@@ -2,23 +2,55 @@
   materialized='table',
 ) }}
 
+WITH sum_cte AS (
+  SELECT 
+    SUM("Internship_internship_ongoing_gp_internship_OnGoing_12_M"::numeric) AS internship_ongoing_12_boys,
+    SUM("Internship_internship_ongoing_gp_internship_OnGoing_11_M"::numeric) AS internship_ongoing_11_boys,
+    SUM("Internship_internship_ongoing_gp_internship_OnGoing_12_F"::numeric) AS internship_ongoing_12_girls,
+    SUM("Internship_internship_ongoing_gp_internship_OnGoing_11_F"::numeric) AS internship_ongoing_11_girls,
+    SUM("Internship_internship_completed_gp_internship_Comp_12_M"::numeric) AS internship_completed_12_boys,
+    SUM("Internship_internship_completed_gp_internship_Comp_12_F"::numeric) AS internship_completed_12_girls,
+    SUM("Internship_internship_completed_gp_internship_Comp_11_F"::numeric) AS internship_completed_11_girls,
+    SUM("Internship_internship_completed_gp_internship_Comp_11_M"::numeric) AS internship_completed_11_boys,
+    SUM("Exit_Survey_Exit_Survey_details_Exit_Survey_Completed_10_M"::numeric) AS exit_survey_completed_10_boys,
+    SUM("Exit_Survey_Exit_Survey_details_Exit_Survey_Completed_12_F"::numeric) AS exit_survey_completed_12_girls,
+    SUM("Exit_Survey_Exit_Survey_details_Exit_Survey_Completed_12_M"::numeric) AS exit_survey_completed_12_boys,
+    SUM("Exit_Survey_Exit_Survey_details_Exit_Survey_Completed_10_F"::numeric) AS exit_survey_completed_10_girls,
+    SUM("re_details_Guest_Lecture_Conducted_12"::numeric) AS guest_lecture_conducted_12,
+    SUM("re_details_Guest_Lecture_Conducted_11"::numeric) AS guest_lecture_conducted_11,
+    SUM("re_details_Guest_Lecture_Conducted_10"::numeric) AS guest_lecture_conducted_10,
+    SUM("re_details_Guest_Lecture_Conducted_9"::numeric) AS guest_lecture_conducted_9
+  FROM {{ ref('pmu_monthly_report') }}
+)
 
-SELECT 
-
-"Internship_internship_ongoing_gp_internship_OnGoing_12_M" as internship_ongoing_12_boys,
-"Internship_internship_ongoing_gp_internship_OnGoing_11_M" as internship_ongoing_11_boys,
-"Internship_internship_ongoing_gp_internship_OnGoing_12_F" as internship_ongoing_12_girls,
-"Internship_internship_ongoing_gp_internship_OnGoing_11_F" as internship_ongoing_11_girls,
-"Internship_internship_completed_gp_internship_Comp_12_M" as internship_completed_12_boys,
-"Internship_internship_completed_gp_internship_Comp_12_F" as internship_completed_12_girls,
-"Internship_internship_completed_gp_internship_Comp_11_F" as internship_completed_11_girls,
-"Internship_internship_completed_gp_internship_Comp_11_M" as internship_completed_11_boys,
-"Exit_Survey_Exit_Survey_details_Exit_Survey_Completed_12_F" as exit_survey_completed_12_girls,
-"Exit_Survey_Exit_Survey_details_Exit_Survey_Completed_12_M" as exit_survey_completed_12_boys,
-"re_details_Guest_Lecture_Conducted_12" as guest_lecture_conducted_12,
-"re_details_Guest_Lecture_Conducted_11" as guest_lecture_conducted_11,
-"re_details_Guest_Lecture_Conducted_10" as guest_lecture_conducted_10,
-"re_details_Guest_Lecture_Conducted_9" as guest_lecture_conducted_9
-
-
-FROM {{ ref('pmu_monthly_report') }}
+SELECT 'internship_ongoing_12_boys' AS category, internship_ongoing_12_boys AS value FROM sum_cte
+UNION ALL
+SELECT 'internship_ongoing_11_boys' AS category, internship_ongoing_11_boys AS value FROM sum_cte
+UNION ALL
+SELECT 'internship_ongoing_12_girls' AS category, internship_ongoing_12_girls AS value FROM sum_cte
+UNION ALL
+SELECT 'internship_ongoing_11_girls' AS category, internship_ongoing_11_girls AS value FROM sum_cte
+UNION ALL
+SELECT 'internship_completed_12_boys' AS category, internship_completed_12_boys AS value FROM sum_cte
+UNION ALL
+SELECT 'internship_completed_12_girls' AS category, internship_completed_12_girls AS value FROM sum_cte
+UNION ALL
+SELECT 'internship_completed_11_girls' AS category, internship_completed_11_girls AS value FROM sum_cte
+UNION ALL
+SELECT 'internship_completed_11_boys' AS category, internship_completed_11_boys AS value FROM sum_cte
+UNION ALL
+SELECT 'exit_survey_completed_12_girls' AS category, exit_survey_completed_12_girls AS value FROM sum_cte
+UNION ALL
+SELECT 'exit_survey_completed_12_boys' AS category, exit_survey_completed_12_boys AS value FROM sum_cte
+UNION ALL
+SELECT 'exit_survey_completed_10_boys' AS category, exit_survey_completed_10_boys AS value FROM sum_cte
+UNION ALL
+SELECT 'exit_survey_completed_10_girls' AS category, exit_survey_completed_10_girls AS value FROM sum_cte
+UNION ALL
+SELECT 'guest_lecture_conducted_12' AS category, guest_lecture_conducted_12 AS value FROM sum_cte
+UNION ALL
+SELECT 'guest_lecture_conducted_11' AS category, guest_lecture_conducted_11 AS value FROM sum_cte
+UNION ALL
+SELECT 'guest_lecture_conducted_10' AS category, guest_lecture_conducted_10 AS value FROM sum_cte
+UNION ALL
+SELECT 'guest_lecture_conducted_9' AS category, guest_lecture_conducted_9 AS value FROM sum_cte
