8ae0560aa7926c9e633dd05a9894daec5b4ab30b
Abhishek-N <abhisheknshetty97@gmail.com>
2023-09-12 18:28:36 +0530

    refactor code and add dbt union

--- models/intermediate/states/chattisgarh_state_database.sql
+++ models/intermediate/states/chattisgarh_state_database.sql
@@ -9,8 +9,8 @@
 
 -- Creating a CTE that flattens the JSON data from the raw_enrollment table
 
-
-({{
+with mycte as (
+{{
     flatten_json(
         model_names = [
                         source('source_lahi', 'chattisgarh_database'), 
@@ -19,3 +19,71 @@
     )
 }})
 
+SELECT 
+    _airbyte_ab_id,
+    NULL as "village",
+    NULL as "s_no",
+    "VC_Email" as "vc_email_id",
+    "HM_Name" as "hm_name",
+    "Block" as "block",
+    "Books_Class_9" as "books_class_9",
+    "Job_Role_For_Class_11_and_12_" as "job_role_for_class_11_and_12",
+    NULL as "remarks",
+    "12Boys" as "12boys",
+    "HM_Email_Id_" as "hm_email_id",
+    "Sector_Trade" as "sector_trade",
+    "11_Total" as "11_total",
+    "10Girls" as "10girls",
+    "Applicable_Classes_09" as "Applicable_Classes_09",
+    "10Boys" as "10boys",
+    "LAB" as "lab",
+    NULL as "po_email_id",
+    "VC_Contact_details" as "vc_phone_number",
+    NULL as "9girls",
+    NULL as "pincode",
+    "VC_Name" as "vc_name",
+    NULL as "cluster",
+    "Year_of_Approval" as "year_of_approval",
+    "School_ID_UDI" as "school_id_udi",
+    "12_Total" as "12_total",
+    NULL as "school_type_name",
+    "State" as "state",
+    "11Boys" as "11boys",
+    NULL as "trade",
+    NULL as "longitude",
+    "Applicable_Classes_11" as "vt_mobile_number",
+    "District" as "district",
+    "VTP" as "vtp",
+    NULL as "approval_year",
+    "9Boys" as "9boys",
+    "Grand_Total" as "grand_total",
+    "12Girls" as "12girls",
+    "Total_Boys" as "total_boys",
+    "HM_Phone_Number" as "hm_phone_number",
+    "Job_Role_For_Class_9_and_10_" as "job_role_for_class_9_and_10_",
+    NULL as "vt_email_id",
+    "Books_Class_11" as "books_class_11",
+    "School_Management" as "school_management",
+    "School_Status" as "school_status",
+    "10_Total" as "10_total",
+    "School_Type" as "school_type",
+    NULL as "po_mob_number",
+    "School_Name" as "school_name",
+    "11Girls" as "11girls",
+    NULL as "po_name",
+    NULL as "school_location_name",
+    NULL as "code_24070101811",
+    NULL as "schoolcategory",
+    NULL as "latitude",
+    "Books_Class_12" as "books_class_12",
+    NULL as "udise_code",
+    "VT_Status" as "vt_status",
+    "Applicable_Classes_12" as "applicable_classes_12",
+    "9_Total" as "9_total",
+    NULL as "school_type_1",
+    "Gender" as "gender",
+    "Books_Class_10" as "books_class_10",
+    "Division" as "division",
+    "Total_Girls" as "total_girls",
+    "Applicable_Classes_10" as "applicable_classes_10"
+FROM mycte
--- models/intermediate/states/karnataka_state_database.sql
+++ models/intermediate/states/karnataka_state_database.sql
@@ -10,7 +10,8 @@
 -- Creating a CTE that flattens the JSON data from the raw_enrollment table
 
 
-({{
+with mycte as (
+{{
     flatten_json(
         model_names = [
                         source('source_lahi', 'karnataka_database'), 
@@ -19,3 +20,72 @@
     )
 }})
 
+
+SELECT 
+    _airbyte_ab_id,
+    NULL as "village",
+    NULL as "s_no",
+    "vc_email_id",
+    "hm_name",
+    "block",
+    "books_class_9",
+    "jobrole_11_12" as "job_role_for_class_11_and_12",
+    NULL as "remarks",
+    NULL as "12boys",
+    "hm_email_id" as "hm_email_id",
+    "sector" as "sector_trade",
+    "_11_total" as "11_total",
+    "class10_g" as "10girls",
+    "school_category",
+    "class10_b" as "10boys",
+    "lab",
+    NULL as "po_email_id",
+    "vc_phone_number",
+    "class9_g" as "9girls",
+    NULL as "pincode",
+    NULL as "vc_name",
+    NULL as "cluster",
+    "year_of_approval",
+    NULL as "school_id_udi",
+    "_12_total",
+    NULL as "school_type_name",
+    "State" as "state",
+    "class11_b" as "11boys",
+    NULL as "trade",
+    NULL as "longitude",
+    "vt_mobile_number",
+    "District" as "district",
+    "vtp",
+    NULL as "approval_year",
+    "class9_b" as "9boys",
+    "grand_total",
+    "class12_g" as "12girls",
+    "total_boys",
+    "hm_phone_number",
+    "jobrole_9_10" as "job_role_for_class_9_and_10_",
+    "vt_email_id",
+    "books_class_11" as "books_class_11",
+    "vt_name",
+    "school_management",
+    "school_status",
+    "_10_total" as "10_total",
+    "school_type",
+    NULL as "po_mob_number",
+    "school_name",
+    "class11_g" as "11girls",
+    NULL as "po_name",
+    NULL as "school_location_name",
+    NULL as "code_24070101811",
+    NULL as "schoolcategory",
+    NULL as "latitude",
+    "books_class_12" as "books_class_12",
+    NULL as "udise_code",
+    "vt_status",
+    "Total_Class_9" as "9_total",
+    NULL as "school_type_1",
+    "gender",
+    "books_class_10" as "books_class_10",
+    "Division" as "division",
+    "total_girls",
+    NULL as "applicable_classes_10"
+FROM mycte
