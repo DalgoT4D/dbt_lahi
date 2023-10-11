143d494d68324bef3433f02a7431a53ef78da67f
Abhishek-N <abhisheknshetty97@gmail.com>
2023-10-11 12:36:51 +0530

    final count of union_state

--- models/prod/aggregated/union_state_aggregated.sql
+++ models/prod/aggregated/union_state_aggregated.sql
@@ -21,22 +21,23 @@ with my_cte as ({{ dbt_utils.union_relations(
 ) }})
 
 SELECT 
+    state,
+    school_status,
+    sector_trade as state_sector,
+    lahi_sector_name as lahi_sector,
+    CASE WHEN "total_boys" ~ '^[0-9\.]+$' THEN "total_boys"::numeric ELSE 0 END as total_boys,
+    CASE WHEN "total_girls" ~ '^[0-9\.]+$' THEN "total_girls"::numeric ELSE 0 END as total_girls,
+    school_category,
+    lab,
+    school_id_udi,
+    vtp,
+    CASE WHEN "grand_total" ~ '^[0-9\.]+$' THEN "grand_total"::numeric ELSE 0 END as grand_total,
+    vt_name,
+    vt_status,
     "state_job_role_11",
     "state_job_role_12",
     state_job_role_11_and_12,
     lahi_job_role_11_and_12,
     state_job_role_9_and_10,
-    lahi_job_role_9_and_10,
-    CASE WHEN "total_boys" ~ '^[0-9\.]+$' THEN "total_boys"::numeric ELSE 0 END as total_boys,
-    CASE WHEN "total_girls" ~ '^[0-9\.]+$' THEN "total_girls"::numeric ELSE 0 END as total_girls,
-    sector_trade as sector,
-    school_category,
-    lab,
-    school_id_udi,
-    state,
-    vtp,
-    CASE WHEN "grand_total" ~ '^[0-9\.]+$' THEN "grand_total"::numeric ELSE 0 END as grand_total,
-    vt_name,
-    school_status,
-    vt_status
+    lahi_job_role_9_and_10
 FROM my_cte
--- models/prod/final/union_state.sql
+++ models/prod/final/union_state.sql
@@ -2,27 +2,68 @@
   materialized='table'
 ) }}
 
-WITH sector_counts AS (
-    SELECT 
-        state,
-        school_status,
-        unnest(array['sector', 'school_category', 'vt_status', 'lab']) as indicator,
-        unnest(array[sector, school_category, vt_status, lab]) as value,
-        COUNT(DISTINCT school_id_udi) as school_count,
-        SUM(total_boys) as total_boys_count,
-        SUM(total_girls) as total_girls_count,
-        SUM(grand_total) as grand_total
-    FROM {{ ref('union_state_aggregated') }}
-    GROUP BY state, school_status, sector, school_category, vt_status, lab
-)
-
-SELECT 
+SELECT
     state,
+    school_id,
+    state_sector,
+    lahi_sector,
+    school_category,
+    vtp,
+    vt_name,
+    lab_status,
+    vt_status,
     school_status,
-    indicator,
-    value,
-    school_count,
-    total_boys_count,
-    total_girls_count,
-    grand_total
-FROM sector_counts
+    CASE 
+        WHEN gender = 'total_boys' THEN 'M'
+        WHEN gender = 'total_girls' THEN 'F'
+    END as gender,
+    COALESCE(
+        CASE 
+            WHEN gender = 'total_boys' THEN total_boys
+            WHEN gender = 'total_girls' THEN total_girls
+            ELSE 0
+        END,
+        0
+    ) as gender_count,
+    state_job_role,
+    lahi_job_role,
+    CASE 
+        WHEN state_job_role_11 IS NOT NULL OR state_job_role_12 IS NOT NULL OR state_job_role_11_and_12 IS NOT NULL OR state_job_role_9_and_10 IS NOT NULL THEN 
+            CASE 
+                WHEN state_job_role_11 IS NOT NULL THEN '11'
+                WHEN state_job_role_12 IS NOT NULL THEN '12'
+                WHEN state_job_role_11_and_12 IS NOT NULL THEN '11-12'
+                WHEN state_job_role_9_and_10 IS NOT NULL THEN '9-10'
+            END
+    END as grade_group
+FROM (
+    SELECT
+        state,
+        school_id_udi as school_id,
+        state_sector,
+        lahi_sector,
+        school_category,
+        vtp,
+        vt_name,
+        lab as lab_status,
+        vt_status,
+        school_status,
+        total_boys,
+        total_girls,
+        unnest(array['total_boys', 'total_girls']) as gender,
+        CASE 
+            WHEN state_job_role_11 IS NOT NULL THEN state_job_role_11
+            WHEN state_job_role_12 IS NOT NULL THEN state_job_role_12
+            WHEN state_job_role_11_and_12 IS NOT NULL THEN state_job_role_11_and_12
+            WHEN state_job_role_9_and_10 IS NOT NULL THEN state_job_role_9_and_10
+        END as state_job_role,
+        CASE 
+            WHEN lahi_job_role_11_and_12 IS NOT NULL THEN lahi_job_role_11_and_12
+            WHEN lahi_job_role_9_and_10 IS NOT NULL THEN lahi_job_role_9_and_10
+        END as lahi_job_role,
+        state_job_role_11,
+        state_job_role_12,
+        state_job_role_11_and_12,
+        state_job_role_9_and_10
+    FROM {{ ref('union_state_aggregated') }}
+) as unnested_data
