e54219277f7f3f6d361378e0e2e0c0f7572eb5d9
Abhishek-N <abhisheknshetty97@gmail.com>
2023-10-13 16:03:57 +0530

    field visit wide table

--- models/prod/aggregated/field_visit_aggregated.sql
+++ models/prod/aggregated/field_visit_aggregated.sql
@@ -18,43 +18,27 @@ WITH cte AS (
             ELSE NULL
         END AS longitude,
         TO_DATE("Date_time_of_Visit", 'YYYY-MM-DD') as date_time_of_visit,
-        date_time_at_the_end_of_visit,
-        EXTRACT(MONTH FROM TO_TIMESTAMP(date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS')) AS month,
-        TRIM(TO_CHAR(TO_TIMESTAMP(date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS'), 'Month')) AS month_name,
-        EXTRACT(DAY FROM TO_TIMESTAMP(date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS')) AS date
+        date_time_at_the_end_of_visit
     FROM {{ref('pmu_field_visit')}}
     GROUP BY 
         id, visit_type, date_time_at_the_end_of_visit, _geolocation, employee_name, State, date_time_of_visit, ve_schools_covered
 )
 
 SELECT 
-    id,
-    State,
-    date_time_of_visit,
-    visit_type,
-    employee_name,
-    ve_schools_covered,
-    month,
-    month_name,
-    date,
-    'latitude' AS geo_type,
-    latitude AS geo
+    cte.id,
+    cte.State,
+    cte.date_time_of_visit,
+    cte.visit_type,
+    cte.employee_name,
+    ed."Project" AS project,
+    cte.ve_schools_covered,
+    EXTRACT(MONTH FROM TO_TIMESTAMP(cte.date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS')) AS month,
+    TRIM(TO_CHAR(TO_TIMESTAMP(cte.date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS'), 'Month')) AS month_name,
+    EXTRACT(DAY FROM TO_TIMESTAMP(cte.date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS')) AS date,
+    cte.latitude,
+    cte.longitude
 FROM 
     cte
-
-UNION ALL
-
-SELECT 
-    id,
-    State,
-    date_time_of_visit,
-    visit_type,
-    employee_name,
-    ve_schools_covered,
-    month,
-    month_name,
-    date,
-    'longitude' AS geo_type,
-    longitude AS geo
-FROM 
-    cte
+LEFT JOIN 
+    {{ ref('employee_data') }} ed ON cte.employee_name = ed."Name"
+    
