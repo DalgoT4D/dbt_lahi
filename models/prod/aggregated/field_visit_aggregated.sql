593d494cef9793513ec1252b6095f926fe983fd2
Abhishek-N <abhisheknshetty97@gmail.com>
2023-10-13 18:24:53 +0530

    update date field

--- models/prod/aggregated/field_visit_aggregated.sql
+++ models/prod/aggregated/field_visit_aggregated.sql
@@ -17,28 +17,27 @@ WITH cte AS (
             WHEN _geolocation IS NOT NULL AND _geolocation != 'null' AND _geolocation != '[null, null]' THEN (regexp_split_to_array(REPLACE(REPLACE(_geolocation, '[', ''), ']', ''), E', '))[2]::numeric
             ELSE NULL
         END AS longitude,
-        TO_DATE("Date_time_of_Visit", 'YYYY-MM-DD') as date_time_of_visit,
+        TO_DATE("Date_time_of_Visit", 'YYYY-MM-DD') as date_of_visit,
         date_time_at_the_end_of_visit
     FROM {{ref('pmu_field_visit')}}
     GROUP BY 
-        id, visit_type, date_time_at_the_end_of_visit, _geolocation, employee_name, State, date_time_of_visit, ve_schools_covered
+        id, visit_type, date_time_at_the_end_of_visit, _geolocation, employee_name, State, date_of_visit, ve_schools_covered
 )
 
 SELECT 
     cte.id,
     cte.State,
-    cte.date_time_of_visit,
+    cte.date_of_visit,
     cte.visit_type,
     cte.employee_name,
     ed."Project" AS project,
     cte.ve_schools_covered,
     EXTRACT(MONTH FROM TO_TIMESTAMP(cte.date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS')) AS month,
     TRIM(TO_CHAR(TO_TIMESTAMP(cte.date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS'), 'Month')) AS month_name,
-    EXTRACT(DAY FROM TO_TIMESTAMP(cte.date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS')) AS date,
+    TO_DATE(cte.date_time_at_the_end_of_visit, 'YYYY-MM-DDTHH24:MI:SS') AS date_at_end_of_visit,
     cte.latitude,
     cte.longitude
 FROM 
     cte
 LEFT JOIN 
     {{ ref('employee_data') }} ed ON cte.employee_name = ed."Name"
-    
--- models/prod/final/field_visit.sql
+++ models/prod/final/field_visit.sql
@@ -12,26 +12,26 @@ WITH cte AS (
         ve_schools_covered,
         latitude,
         longitude,
-        date_time_of_visit,
+        date_of_visit,
         month,
         month_name,
-        date
+        date_at_end_of_visit
     FROM {{ref('field_visit_aggregated')}}
     GROUP BY 
-        id, visit_type, latitude, longitude, month, month_name, date, employee_name, project, State, date_time_of_visit, ve_schools_covered
+        id, visit_type, latitude, longitude, month, month_name, date_at_end_of_visit, employee_name, project, State, date_of_visit, ve_schools_covered
 )
 
 SELECT 
     id,
     State,
-    date_time_of_visit,
+    date_of_visit,
     visit_type,
     employee_name,
     project,
     ve_schools_covered,
     month,
     month_name,
-    date,
+    date_at_end_of_visit,
     'latitude' AS geo_type,
     latitude AS geo
 FROM 
@@ -42,14 +42,14 @@ UNION ALL
 SELECT 
     id,
     State,
-    date_time_of_visit,
+    date_of_visit,
     visit_type,
     employee_name,
     project,
     ve_schools_covered,
     month,
     month_name,
-    date,
+    date_at_end_of_visit,
     'longitude' AS geo_type,
     longitude AS geo
 FROM 
