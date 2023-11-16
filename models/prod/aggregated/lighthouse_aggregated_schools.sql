6143fd04186fa347e5002802ef70e349a5eb2aa6
Abhishek-N <abhisheknshetty97@gmail.com>
2023-11-16 19:55:23 +0530

    aggregated table for lighthouse schools

--- models/prod/aggregated/lighthouse_aggregated_schools.sql
+++ models/prod/aggregated/lighthouse_aggregated_schools.sql
@@ -1,28 +1,24 @@
-{{ config(
-  materialized='table'
-) }}
-
 SELECT
-    S.udise AS school_id_udi,
-    S.schoolname AS school_name,
-    SC.fullname as student_name,
+    ST.statename AS state,
+    S.isactive AS school_status
+    SE.sectorname AS sector_name,
     CASE 
-        WHEN CAST(SC.gender AS INTEGER) = 207 THEN 'M'
-        WHEN CAST(SC.gender AS INTEGER) = 208 THEN 'F'
+        WHEN CAST(C.gender AS INTEGER) = 207 THEN 'M'
+        WHEN CAST(C.gender AS INTEGER) = 208 THEN 'F'
         ELSE 'Unknown' 
     END AS gender,
-    AY.yearname as yearname,
     SHC.categoryname AS school_category,
-    ST.statename AS state,
-    SE.sectorname AS sector_name,
+    TE.testatus AS lab_status,
+    S.udise AS school_id_udi,
     VTP.vtpname AS vtp,
+    COUNT(SC.studentid) AS total_students,
+    AY.yearname as yearname,
     VT.fullname AS vt_name,
     VT.isactive AS vt_status,
-    JR.jobrolename AS job_role,
-    TE.testatus AS lab_status,
-    COUNT(SC.studentid) AS total_students
+    JR.jobrolename AS job_role
 FROM {{ ref('schools') }} S
-LEFT JOIN {{ ref('student_classes') }} SC ON S.schoolid = SC.schoolid
+LEFT JOIN {{ ref('student_class_mapping') }} SC ON S.schoolid = SC.schoolid
+LEFT JOIN {{ ref('student_classes') }} C ON S.schoolid = C.schoolid
 LEFT JOIN {{ ref('academic_years') }} AY ON SC.academicyearid = AY.academicyearid
 LEFT JOIN {{ ref('school_categories') }} SHC ON S.schoolcategoryid = SHC.schoolcategoryid
 LEFT JOIN {{ ref('states') }} ST ON S.statecode = ST.statecode
@@ -33,5 +29,5 @@ LEFT JOIN {{ ref('vocational_trainers') }} VT ON VCS.vtid = VT.vtid
 LEFT JOIN {{ ref('vocational_training_providers') }} VTP ON VT.vtpid = VTP.vtpid
 LEFT JOIN {{ ref('job_roles') }} JR ON SCD.jobroleid = JR.jobroleid
 LEFT JOIN {{ ref('tool_equipments') }} TE on S.schoolid = TE.schoolid
--- WHERE AY.yearname='2022-2023'
-GROUP BY S.udise, SC.fullname, SC.gender, S.schoolname, AY.yearname, SHC.categoryname, ST.statename, SE.sectorname, VTP.vtpname, VT.fullname, VT.isactive, JR.jobrolename, TE.testatus
+where SC.isactive=true
+GROUP BY S.udise, S.schoolname,S.isactive, SC.isactive, AY.yearname, SHC.categoryname, ST.statename, SE.sectorname, VTP.vtpname, VT.fullname, VT.isactive, JR.jobrolename, TE.testatus
